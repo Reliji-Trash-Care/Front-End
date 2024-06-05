01255114f0f47940894643ca3462d0df0fb10ddb
Ntune0241 <129527507+Ntune0241@users.noreply.github.com>
2024-06-05 07:11:54 +0700

    memperbaiki setelan

--- src/screens/SetelanGanti/SetelanGanti.jsx
+++ src/screens/SetelanGanti/SetelanGanti.jsx
@@ -18,7 +18,7 @@ export const SetelanGanti = () => {
             Edit Profile</a>
             <a href="/setelanganti" className="font-body-1-regular w-fit tracking-[var(--body-1-regular-letter-spacing)] [font-style:var(--body-1-regular-font-style)] text-[length:var(--body-1-regular-font-size)] text-black font-[number:var(--body-1-regular-font-weight)] text-center whitespace-nowrap leading-[var(--body-1-regular-line-height)] relative">
             Ganti</a>
-            <a href="/setelanganti" className="font-body-1-regular w-fit tracking-[var(--body-1-regular-letter-spacing)] [font-style:var(--body-1-regular-font-style)] text-[length:var(--body-1-regular-font-size)] text-black font-[number:var(--body-1-regular-font-weight)] text-center whitespace-nowrap leading-[var(--body-1-regular-line-height)] relative">
+            <a href="/setelannotifikasi" className="font-body-1-regular w-fit tracking-[var(--body-1-regular-letter-spacing)] [font-style:var(--body-1-regular-font-style)] text-[length:var(--body-1-regular-font-size)] text-black font-[number:var(--body-1-regular-font-weight)] text-center whitespace-nowrap leading-[var(--body-1-regular-line-height)] relative">
             Notifikasi</a>
           </div>
           <div className="inline-flex flex-col items-start gap-[24px] absolute top-[32px] left-[427px]">
--- src/screens/SetelanNotifikasi/SetelanNotifikasi.jsx
+++ src/screens/SetelanNotifikasi/SetelanNotifikasi.jsx
@@ -18,7 +18,7 @@ export const SetelanNotifikasi = () => {
             Edit Profile</a>
             <a href="/setelanganti" className="font-body-1-regular w-fit tracking-[var(--body-1-regular-letter-spacing)] [font-style:var(--body-1-regular-font-style)] text-[length:var(--body-1-regular-font-size)] text-black font-[number:var(--body-1-regular-font-weight)] text-center whitespace-nowrap leading-[var(--body-1-regular-line-height)] relative">
             Ganti</a>
-            <a href="/setelanganti" className="font-body-1-regular w-fit tracking-[var(--body-1-regular-letter-spacing)] [font-style:var(--body-1-regular-font-style)] text-[length:var(--body-1-regular-font-size)] text-black font-[number:var(--body-1-regular-font-weight)] text-center whitespace-nowrap leading-[var(--body-1-regular-line-height)] relative">
+            <a href="/setelannotifikasi" className="font-body-1-regular w-fit tracking-[var(--body-1-regular-letter-spacing)] [font-style:var(--body-1-regular-font-style)] text-[length:var(--body-1-regular-font-size)] text-black font-[number:var(--body-1-regular-font-weight)] text-center whitespace-nowrap leading-[var(--body-1-regular-line-height)] relative">
             Notifikasi</a>
           </div>
           <div className="inline-flex flex-col items-start gap-[24px] absolute top-[35px] left-[429px]">
